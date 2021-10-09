8897b56ef49059d245052d3775352933e45242af
dMosca <devinmosca@yahoo.com>
2021-10-08 21:45:04 -0600

    Server works, CSS not working, only main handlebars work

--- server.js
+++ server.js
@@ -34,7 +34,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
+
+app.get('/', (req, res) => {
+  res.render('main');
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
