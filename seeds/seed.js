9325e8262fecc261b501522f41256fa82872e018
dMosca <devinmosca@yahoo.com>
2021-10-12 13:00:28 -0600

    Checked seed file, removed comment seed temporarily

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Blog, Comment } = require('../models');
+const { User, Blog } = require('../models');
 
 const userData = require('./userData.json');
 const blogData = require('./blogData.json');
-const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -16,15 +15,7 @@ const seedDatabase = async () => {
   for (const blog of blogData) {
     await Blog.create({
       ...blog,
-      blogger_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
-
-  for (const comment of commentData) {
-    await Comment.create({
-      ...comment,
-      readerComment_id: users[Math.floor(Math.random() * users.length)].id,
-      blogComment_id: users[Math.floor(Math.random() * users.length)].id,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
 
