a5a3b9116aa5de463b297dbb880fe63876f3647e
dMosca <devinmosca@yahoo.com>
2021-10-12 13:24:25 -0600

    Redid userRoutes and blogRoutes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,9 +1,3 @@
-// Add the withauth from this to the userRoutes. 
-// After routes are done should be able to run the server and see if this garbage works!!
-// Need to do a put route
-// Need to do post, put, delete route for comments 
-
-
 const router = require('express').Router();
 const { Blog } = require('../../models');
 const withAuth = require('../../utils/auth');
@@ -12,8 +6,9 @@ router.post('/', withAuth, async (req, res) => {
   try {
     const newBlog = await Blog.create({
       ...req.body,
-      blogger_id: req.session.user_id,
+      user_id: req.session.user_id,
     });
+
     res.status(200).json(newBlog);
   } catch (err) {
     res.status(400).json(err);
@@ -25,7 +20,7 @@ router.delete('/:id', withAuth, async (req, res) => {
     const blogData = await Blog.destroy({
       where: {
         id: req.params.id,
-        blogger_id: req.session.user_id,
+        user_id: req.session.user_id,
       },
     });
 
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,21 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.get('/', async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      include: [
-        {
-          attributes: ['user_name']
-        }
-      ]
-    })
-    console.log(userData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-})
-
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
