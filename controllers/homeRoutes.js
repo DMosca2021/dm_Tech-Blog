6243d7a105e3b86518262510f03077572cff15a8
dMosca <devinmosca@yahoo.com>
2021-10-13 16:47:42 -0600

    Added route for account create, works on local host

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -79,4 +79,14 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/create', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('create');
+});
+
 module.exports = router;
